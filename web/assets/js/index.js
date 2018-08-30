4222c966d4b5495917817a38ab2b5d3307ecde80
superj80820 <superj80820@gmail.com>
2018-08-30 02:36:17 +0000

    將邀請碼換成cookie

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,9 +1,22 @@
-var myWindow;
-
 function sentid() {
-    myWindow = window.open("home.html");
+	window.location.href="https://messfar.com/line_saying/home.html";
+ 
+//window.open("https://messfar.com/line_saying/");
+  //  window.open('','_top'); 
+   // window.top.close(); 
+//    window.location = "https://messfar.com/line_saying/home.html" 
+  //  window.open("home.html");
+//window.close();
+  //  window.open("index.html");
+	var sentid=document.getElementById('SentId').value;
+	$.cookie("LineSayId",sentid);
+	console.log(sentid);
+
+	window.location.href="https://messfar.com/line_saying/home.html";
 }
 
-function closeWin() {
-    myWindow.close("index.html");
+function getid(){
+	var getid=$.cookie("LineSayId");
+	console.log(getid);
 }
+

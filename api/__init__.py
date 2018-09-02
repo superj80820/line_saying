11425a1d7d3b75abb10e9071260aecffd85b0487
York Lin <superj80820@gmail.com>
2018-09-03 01:37:50 +0800

    新增傳圖功能

--- api/__init__.py
+++ api/__init__.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 #圖片新增的排序是否為最新 須測試(應該是正常)
 #vote選項請勿使用 , 不然可能會錯誤
+#目前傳送圖片的功能只有針對jpg
 
 import string
 import json
 import re
 import random
+import time
 import ast
 import requests
 import pandas
@@ -108,16 +110,34 @@ def handle_message(event):
         conn.close()
 
     elif event.message.text == "/public_no":
+        user_name=line_bot_api.get_profile(event.source.user_id).display_name
+
+        conn = sqlite.connect('%sdata/db/create_check.db'%(FileRout))
+        c = conn.cursor()
+        c.execute('UPDATE user_in_where SET public_name ="no" WHERE id ="%s"'%(event.source.user_id))
+        c.execute('UPDATE user_in_where SET user_name ="%s" WHERE id ="%s"'%(user_name,event.source.user_id))
         line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='好~以匿名~'))
+        conn.commit()
+        conn.close()
 
     elif event.message.text[0] == '?':
         say=event.message.text[1:len(event.message.text)]
-    
+
+        conn = sqlite.connect('%sdata/db/create_check.db'%(FileRout))
+        c = conn.cursor()
+        public_name = c.execute('SELECT public_name FROM user_in_where WHERE id ="%s"'%(event.source.user_id))
+        public_name =public_name.fetchall()[0][0]
+        if public_name == 'yes':
+            user_name = c.execute('SELECT user_name FROM user_in_where WHERE public_name ="yes" AND id ="%s"'%(event.source.user_id))
+            user_name = user_name.fetchall()[0][0]
+        elif public_name == 'no':
+            user_name = '匿名'
+
         conn = sqlite.connect('%sdata/db/%s.db'%(FileRout,USER_GET_MEET_ID(event.source.user_id)))
         c = conn.cursor()
-        c.execute('INSERT INTO user_say (id,say,timestamp) VALUES ("%s","%s","%s")'%(event.source.user_id,say,str(event.timestamp)))
+        c.execute('INSERT INTO user_say (name,id,say,timestamp) VALUES ("%s","%s","%s","%s")'%(user_name,event.source.user_id,say,str(event.timestamp)))
         conn.commit()
         conn.close()
         line_bot_api.reply_message(
@@ -150,7 +170,7 @@ def handle_content_message(event):
     conn = sqlite.connect('%sdata/db/%s.db'%(FileRout,USER_GET_MEET_ID(event.source.user_id)))
     c = conn.cursor()
     get_all_timestamp=c.execute('SELECT timestamp FROM user_say WHERE id ="%s"'%(event.source.user_id)).fetchall()
-    c.execute('UPDATE user_say SET image ="https://messfar.com/line_saying/%s.jpg" WHERE timestamp ="%s"'%(str(event.timestamp),get_all_timestamp[len(get_all_timestamp)-1][0]))
+    c.execute('UPDATE user_say SET image ="https://messfar.com/line_saying_data/%s.jpg" WHERE timestamp ="%s"'%(str(event.timestamp),get_all_timestamp[len(get_all_timestamp)-1][0]))
     conn.commit()
     conn.close()
     line_bot_api.reply_message(
@@ -210,7 +230,7 @@ def create_meet():
     def meet_data(invite_id,web_id,slide_key,meet_name):
         conn = sqlite.connect('%sdata/db/%s.db'%(FileRout,web_id))
         c = conn.cursor()
-        data = 'id TEXT,say TEXT,timestamp TEXT,image TEXT'
+        data = 'name TEXT,id TEXT,say TEXT,timestamp TEXT,image TEXT'
         c.execute('CREATE TABLE info(meet_name TEXT,invite_id TEXT,web_id TEXT,slide_key TEXT)')
         c.execute('CREATE TABLE user_say(%s)'%(data))
         c.execute('CREATE TABLE vote_sort(sort INTEGER PRIMARY KEY AUTOINCREMENT,vote_id TEXT NOT NULL)')
@@ -237,7 +257,7 @@ def create_meet():
             invite_id = c.execute('SELECT invite_id FROM meet_check WHERE api_request ="%s"'%(web_id))
             invite_id = invite_id.fetchall()[0][0]
             meet_data(invite_id,web_id,slide_key,meet_name)
-            line_bot_api.push_message(sent_id, TextSendMessage(text='已驗證成功~'))
+            line_bot_api.push_message(sent_id, TextSendMessage(text='已驗證成功~\n邀請碼是%s'%(invite_id)))
             conn.commit()
             conn.close()
 
@@ -335,17 +355,46 @@ def say():
  
     return 'ok'
 
+
+@app.route('/sent_image', methods=['POST'])
+def sent_image():
+    meet_id = request.form['meet_id']
+    image = request.files['image']
+    timestamp = str(int(time.time()))
+
+    image.save('%sdata/image/speech/%s.jpg'%(FileRout,timestamp))
+    image_url = 'https://messfar.com/line_saying_data/speech/%s.jpg'%(timestamp)
+    
+    conn = sqlite.connect('%sdata/db/create_check.db'%(FileRout))
+    c = conn.cursor()
+    user_id = c.execute('SELECT id FROM user_in_where WHERE meet ="%s"'%(meet_id))
+    user_id = user_id.fetchall()
+    
+    for item in user_id:
+        headers = {'Content-Type':'application/json','Authorization':'Bearer %s'%(line_token)}
+        payload = {
+            'to':item[0],
+            'messages':[{"type": "image","originalContentUrl": "%s"%(image_url),"previewImageUrl": "%s"%(image_url)}]
+            }
+        res=requests.post('https://api.line.me/v2/bot/message/push',headers=headers,json=payload)
+    
+    conn.commit()  
+    conn.close()
+ 
+    return 'ok'
+
 @app.route('/user_say', methods=['GET'])
 def user_say():
     meet_id=request.args.get('meet_id')
     
     conn = sqlite.connect('%sdata/db/%s.db'%(FileRout,meet_id))
     df = pandas.read_sql_query("SELECT * FROM user_say", conn)
-    print(df.to_dict(orient='records'))
+    ret = df.to_dict(orient='records')
+
     conn.commit()  
     conn.close()
  
-    return jsonify(df.to_dict(orient='records'))
+    return jsonify(ret)
 
 @app.route('/vote', methods=['GET'])
 def get_vote():

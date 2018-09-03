6ff0ab6eba9c31d3e7be07f83179265fb4da1d2c
York Lin <superj80820@gmail.com>
2018-09-03 23:09:43 +0800

    修復index功能

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,5 +1,5 @@
-function sentid() {
-	window.location.href="https://messfar.com/line_saying/home.html";
+function sent_id() {
+	// window.location.href="https://messfar.com/line_saying/home.html";
  
 //window.open("https://messfar.com/line_saying/");
   //  window.open('','_top'); 
@@ -8,15 +8,25 @@ function sentid() {
   //  window.open("home.html");
 //window.close();
   //  window.open("index.html");
-	var sentid=document.getElementById('SentId').value;
-	$.cookie("LineSayId",sentid);
-	console.log(sentid);
+	var meet_name=document.getElementById('MeetName').value;
+	var sent_id=document.getElementById('SentId').value;
+	var slide_link=document.getElementById('SlideLink').value;
+	$.cookie("MeetName",meet_name);
+	$.cookie("SentId",sent_id);
+	$.cookie("SlideLink",slide_link);
+	console.log(meet_name);
+	console.log(sent_id);
+	console.log(slide_link);
 
 	window.location.href="https://messfar.com/line_saying/home.html";
 }
 
 function getid(){
-	var getid=$.cookie("LineSayId");
-	console.log(getid);
+	var meet_name=$.cookie("MeetName");
+	var sent_id=$.cookie("SentId");
+	var slide_link=$.cookie("SlideLink");
+	console.log(meet_name);
+	console.log(sent_id);
+	console.log(slide_link);
 }
 

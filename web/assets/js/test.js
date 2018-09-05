683944b3cd32d6b8e0b3fed3b7f6b80f458648d3
York Lin <superj80820@gmail.com>
2018-09-06 01:50:30 +0800

    投票大致完成(只有兩個選項)

--- web/assets/js/test.js
+++ web/assets/js/test.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var meet_name=$.cookie("MeetName");
+  var sent_id=$.cookie("SentId");
+  var slide_link=$.cookie("SlideLink");
+  var invite_id=$.cookie("InviteId");
+  var slide_key=$.cookie("SlideKey");
+
   var $ul = $("ul[class='va']");
   var a,a1,a2;
   var flag=false;
@@ -7,7 +13,7 @@ $(document).ready(function() {
   var n;
   $.ajax({
     type: 'GET',
-    url: 'https://lis.apps.exosite.io/api:1/lecture/01161fc5ff/vote',
+    url: 'https://messfar.com/line_saying_api/vote?meet_id='+sent_id,
     dataType: 'json',
       success: function(test_dic) {
         var i;
@@ -20,40 +26,42 @@ $(document).ready(function() {
           $('#allvote').hide();
         }
         for (i=1;i<=n;i++){
-          item = '<h3>'+test_dic[i-1].template['text']+'<b style=font-size:15px>&emsp;<span id=person'+i+'></span>&nbsp&nbspperson voted&nbsp&nbsp&nbsp&nbsp<input type=button id=sAdd value=Add></b></h3><span id=T'+i+'_option1></span></div><div><span id=T'+i+'_option2></span>';
+          item = '<h3>'+test_dic[i-1].vote_name+'<b style=font-size:15px>&emsp;<span id=person'+i+'></span>&nbsp&nbspperson voted&nbsp&nbsp&nbsp&nbsp<input type=button id=sAdd value=Add></b></h3><span id=T'+i+'_option1></span></div><div><span id=T'+i+'_option2></span>';
           $ul.append(item);
-          d1 = test_dic[i-1].template['actions'][0]['label']
-          d2 = test_dic[i-1].template['actions'][1]['label']
+          d1 = test_dic[i-1].vote_item[0]['label']
+          d2 = test_dic[i-1].vote_item[1]['label']
+          a1 = test_dic[i-1].vote_item[0]['vote_count']
+          a2 = test_dic[i-1].vote_item[1]['vote_count']
+          a = parseInt(a1)+parseInt(a2);
+          s1 = "#T"+i+"_option1";
+          s2 = "#T"+i+"_option2";
+          console.log(d1)
+          console.log(d2)
+          $(s1).html("<input type=radio name=vote"+i+"_tite1 checked><label>"+d1+"&nbsp&nbsp&nbsp&nbsp"+a1+"/"+a+"</label>");
+          $(s2).html("<input type=radio name=vote"+i+"_tite2><label>"+d2+"&nbsp&nbsp&nbsp&nbsp"+a2+"/"+a+"</label>");
           var id = test_dic[i-1].voteId
-          getBody(id,i,d1,d2);
+          // getBody(id,i,d1,d2);
         }
       }
   }); 
   
   
   function getBody(id,i,d1,d2){
-    $.ajax({
-      type: 'GET',
-      url: 'https://lis.apps.exosite.io/api:1/lecture/01161fc5ff/vote/'+id+'/count',
-      dataType: 'json',
-        success: function(test_j) {
-          a1 = parseInt(test_j['01161fc5ff::'+id+'::a'])
-          a2 = parseInt(test_j['01161fc5ff::'+id+'::b'])
-          s1 = "#T"+i+"_option1";
-          s2 = "#T"+i+"_option2";
-          a = a1+a2
-          if (a1 > a2){
-            $(s1).html("<input type=radio name=vote"+i+"_tite1 checked><label>"+d1+"&nbsp&nbsp&nbsp&nbsp"+a1+"/"+a+"</label>");
-            $(s2).html("<input type=radio name=vote"+i+"_tite2><label>"+d2+"&nbsp&nbsp&nbsp&nbsp"+a2+"/"+a+"</label>");
-          }
-          else{
-            $(s1).html("<input type=radio name=vote"+i+"_tite1><label>"+d1+"&nbsp&nbsp&nbsp&nbsp"+a1+"/"+a+"</label>");
-            $(s2).html("<input type=radio name=vote"+i+"_tite2 checked><label>"+d2+"&nbsp&nbsp&nbsp&nbsp"+a2+"/"+a+"</label>");
-          }
-          s = "#person"+i;
-          $(s).text(a);
-        }
-    });
+    a1 = parseInt(test_j['01161fc5ff::'+id+'::a'])
+    a2 = parseInt(test_j['01161fc5ff::'+id+'::b'])
+    s1 = "#T"+i+"_option1";
+    s2 = "#T"+i+"_option2";
+    a = a1+a2
+    if (a1 > a2){
+      $(s1).html("<input type=radio name=vote"+i+"_tite1 checked><label>"+d1+"&nbsp&nbsp&nbsp&nbsp"+a1+"/"+a+"</label>");
+      $(s2).html("<input type=radio name=vote"+i+"_tite2><label>"+d2+"&nbsp&nbsp&nbsp&nbsp"+a2+"/"+a+"</label>");
+    }
+    else{
+      $(s1).html("<input type=radio name=vote"+i+"_tite1><label>"+d1+"&nbsp&nbsp&nbsp&nbsp"+a1+"/"+a+"</label>");
+      $(s2).html("<input type=radio name=vote"+i+"_tite2 checked><label>"+d2+"&nbsp&nbsp&nbsp&nbsp"+a2+"/"+a+"</label>");
+    }
+    s = "#person"+i;
+    $(s).text(a);
   }
 
   $(document).on('click', '#Vadd', function() {
@@ -72,41 +80,43 @@ $(document).ready(function() {
   }); 
 
   $(document).on('click', '#send', function() {
+    var meet_name=$.cookie("MeetName");
+    var sent_id=$.cookie("SentId");
+    var slide_link=$.cookie("SlideLink");
+    var invite_id=$.cookie("InviteId");
+    var slide_key=$.cookie("SlideKey");
+
     var id = '01161fc5ff';
     
     $.ajax({
       type: 'POST',
-      url: 'https://lis.apps.exosite.io/api:1/lecture/'+id+'/vote',
+      url: 'https://messfar.com/line_saying_api/vote',
       contentType: "application/json; charset=utf-8",
       dataType: 'json',
       data: JSON.stringify({
-        "type": "template",
-        "altText": "This is a buttons template",
-        "template": {
+        "meet_id":sent_id,
+        "vote_data":{
+          "type": "template",
+          "altText": "This is a buttons template",
+          "template": {
             "type": "buttons",
             "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover",
-            "imageBackgroundColor": "#FFFFFF",
-            "title": "投票",
-            "text": $('#title_na').val(),
-            "defaultAction": {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
-            },
+            "title": $('#title_na').val(),
+            "text": "暫無設定",
             "actions": [{
                     "type": "postback",
                     "label": $('#title_op1').val(),
-                    "data": "{lectureId}::{voteId}::a"
+                    "data": "{'type':'vote','index':'1','label':'"+$('#title_op1').val()+"'}"
                 },
                 {
                     "type": "postback",
                     "label": $('#title_op2').val(),
-                    "data": "{lectureId}::{voteId}::b"
+                    "data": "{'type':'vote','index':'2','label':'"+$('#title_op2').val()+"'}"
                 }
             ]
+          }
         }
+        
     }),
       complete: function(test_dic) {        
         window.location.href = "success.html";
